48ea349cdb9d7e2d002b4767dd464333cf7db8ab
Tammy Faga <tammy@superaremedia.com.br>
2018-07-18 01:51:56 -0300

    Insere hover com likes e ordena ranking

--- src/js/load.js
+++ src/js/load.js
@@ -7,16 +7,11 @@ var App = App || {};
 		init: function() {
 			this.cacheSelectors();
 			this.Events.loadData();
-			// this.bindEvents();
 		},
 
 		cacheSelectors: function() {
 			this.list = $('.ul-list');
 		},
-
-		// bindEvents: function bindEvents() {
-
-		// },
 
 		Events: {
 
@@ -30,14 +25,34 @@ var App = App || {};
 				httpRequest.addEventListener("readystatechange", function () {
 					if (httpRequest.readyState == 4){
 						if (httpRequest.status == 200){
-							var response = httpRequest.response.data;
+							var data = httpRequest.response.data;
+							var response = data.sort(function(a, b) {
+								return Number(b.positive) - Number(a.positive);
+							});
+							
 							var template = '';
+							
+							console.log (response);
 
 								response.forEach(function(response){
 									var img = response.picture;
-									template += '<li class="li-item"><div class="circle-img"><img src='+img+'></div><div class="description"><h2 class="title">'+ response.name +'</h2><p class="text">' +decodeURIComponent(response.description)+'</p></div></li>';
+									var totalLikes = Number(response.positive) + Number(response.negative);
+									var positive = Math.round(Number(response.positive) / totalLikes * 100);
+									var negative = Math.round(Number(response.negative) / totalLikes * 100);
+
+									if (isNaN(positive)) {
+										positive = 0;
+									}
+
+									if (isNaN(negative)) {
+										negative = 0;
+									}
+									
+									template += '<li class="li-item"><div class="circle-img"><img src='+img+'></div><div class="description"><h2 class="title">'+ response.name +'</h2><p class="text">' +decodeURIComponent(response.description)+'</p></div><div class="likes"><div class="likes-box"><div class="likes-box-header"><h3 class="title">Gostam</h3></div><div class="likes-box-status"><p class="text">'+positive+'%</p></div></div><div class="likes-box"><div class="likes-box-header"><h3 class="title">Não Gostam</h3></div><div class="likes-box-status"><p class="text">'+negative+'%</p></div></div></div>'
 								});
+
 								self.list.append(template);
+								
 						} else {
 							return alert('Não foi possível carregar as informações');  
 						}
